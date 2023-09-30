6371940e83407aeaba462cda9f2e8bd8606aa662
Fareed Khan <127206523+Fareedkhan56@users.noreply.github.com>
2023-09-30 17:49:51 +0500

    Add files via upload

---
+++ access modifiers/program/program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class MyCar
+    {
+        public string model;
+        public string color;
+        public int price;
+        public void car()
+        {
+            Console.WriteLine("Model : "+model);
+            Console.WriteLine("Color : " + color);
+            Console.WriteLine("Price : " + price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCar car = new MyCar();
+            Console.Write("enter car model : ");
+            car.model = Console.ReadLine();
+            Console.Write("enter car color : ");
+            car.color = Console.ReadLine();
+            Console.Write("enter car price : ");
+            car.price = Convert.ToInt32(Console.ReadLine());
+            car.car();
+
+            Console.ReadLine();
+        }
+    }
+}
